6df4047a1f602dac425748709dc44ee3e66cd76b
cfviotti <caio@viotti.me>
2024-08-14 12:07:07 -0300

    Setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  site: 'https://caio.viotti.me/',
+  site: "https://caio.viotti.me/",
+  integrations: [tailwind()],
 });
